1ad41a8fe8424c1cc8f6d282aa638a348aa79e91
Iain Wentworth <iain.wentworth@gmail.com>
2018-12-18 22:26:44 +0000

    Second commit

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -4,13 +4,30 @@ const Dinosaur = require('../models/dinosaur.js');
 
 describe('Park', function() {
 
+let park;
+let dinosaur1;
+let dinosaur2;
+let dinosaur3;
+
   beforeEach(function () {
+    dinosaur1 = new Dinosaur('t-rex', 'carnivore', 50);
+    dinosaur2 = new Dinosaur('velociraptor', 'carnivore', 30);
+    dinosaur3 = new Dinosaur('triceratops', 'herbivore', 40);
 
-  })
+    dinosaurs = [dinosaur1, dinosaur2, dinosaur3]
 
-  it('should have a name');
+    park = new Park('Jurrasic Park', 100, dinosaurs);
+  });
 
-  it('should have a ticket price');
+  it('should have a name', function () {
+    const actual = park.name;
+    assert.strictEqual(actual, 'Jurrasic Park');
+  });
+
+  it('should have a ticket price', function () {
+    const actual = park.price;
+    assert.strictEqual(actual, 100);
+  });
 
   it('should have a collection of dinosaurs');
 
